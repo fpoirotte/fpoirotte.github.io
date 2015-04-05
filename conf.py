f7b5d10bc104a1f7dc1a263196c28e9f7b9af7b4
François Poirotte <clicky@erebot.net>
2015-04-06 01:49:18 +0200

    Add Piwik analytics code.

--- conf.py
+++ conf.py
@@ -259,7 +259,9 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'analytics_id': 6,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [alabaster.get_path()]
